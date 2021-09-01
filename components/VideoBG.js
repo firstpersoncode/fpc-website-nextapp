b0fdc6eb3f14e409f8dc93bf39b789cc243ff135
firstpersoncode <nasser.maronie@gmail.com>
2021-09-01 13:59:01 +0800

    add lazy loading video

--- components/VideoBG.js
+++ components/VideoBG.js
@@ -3,13 +3,14 @@ export default function ComponentVideoBG({ video }) {
 
   return (
     <div className="relative h-screen overflow-hidden">
-      {video && video.url ? <video autoPlay muted loop playsinline className="lazy min-h-full min-w-full absolute" style={{
+      {video && video.url ? <video autoPlay muted loop playsInline className="lazy min-h-full min-w-full absolute" style={{
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
         objectFit: "cover",
       }}>
-        <source data-src={STRAPI_HOST + video.url} type="video/mp4" />
+        <source src={STRAPI_HOST + video.url} data-src={STRAPI_HOST + video.url} type="video/mp4" />
+        Your browser doesn't support HTML5 video
       </video> : null}
       <div className="absolute w-full h-full inset-0" style={{
         backgroundColor: "rgba(0,0,0,0.75)"
