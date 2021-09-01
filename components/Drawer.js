75c497959dd1c4dd01b5adf6a97ae6ea9ce6064f
firstpersoncode <nasser.maronie@gmail.com>
2021-09-01 13:31:56 +0800

    tidy up drawer

--- components/Drawer.js
+++ components/Drawer.js
@@ -2,14 +2,15 @@ export default function ComponentDrawer({ open, onClose, children }) {
   return (
     <>
       {open ? <div onClick={onClose} className="fixed z-50 inset-0 w-full h-full" style={{ backgroundColor: "rgba(0,0,0,0.5)" }} /> : null}
-      <div className="fixed bottom-0 left-0 right-0 w-full transition duration-500 ease-in-out z-50 overflow-y-auto" style={{
+      <div className="fixed bottom-0 left-0 right-0 w-full transition duration-500 ease-in-out z-50" style={{
         transform: open ? "translateY(0)" : "translateY(100%)",
-        opacity: open ? 1 : 0,
-        maxHeight: "40vh"
+        opacity: open ? 1 : 0
       }}>
         <div className="relative p-10 pb-20 bg-white rounded">
           <button onClick={onClose} className="absolute border border-gray-300 rounded border-b-4 right-0 left-0 m-auto top-4" style={{ width: 100 }}></button>
-          {children}
+          <div className="overflow-y-auto" style={{
+            maxHeight: "40vh"
+          }}>{children}</div>
         </div>
       </div>
     </>
