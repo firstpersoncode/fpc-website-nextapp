5d16414d076ecbde458fe3c497ea7974119c0064
firstpersoncode <nasser.maronie@gmail.com>
2021-09-01 11:55:01 +0800

    always set dev to false

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev: true })
+// const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev: false })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
